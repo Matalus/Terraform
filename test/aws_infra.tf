a6a65bdd7ceef6b94abe0fbedf3c7cf00dd0883d
Matt Hamende <matt.hamende@gmail.com>
2018-11-02 09:58:15 -0700

    adding IGW and EIPs

--- test/aws_infra.tf
+++ test/aws_infra.tf
@@ -1,27 +1,49 @@
 #Define provider Amazon Web Services
 provider "aws" {
    region = "us-east-2"
-   access_key = "${var.AWS_ACCESS_KEY}"
-   secret_key = "${var.AWS_SECRET_KEY}"
+   access_key = "${var.AWS_ACCESS_KEY}" #configured in tfvars
+   secret_key = "${var.AWS_SECRET_KEY}" #configured in tfvars
    
 }
 #define VPC 
 resource "aws_vpc" "tftest" {
     cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = "true"
+    enable_dns_support = "true"
+    tags {
+        Name = "tf-vpc"
+    }
 }
 resource "aws_internet_gateway" "gw" {
     vpc_id = "${aws_vpc.tftest.id}"
+
+    tags {
+        Name = "infra_tf"
+    }
 }
 
 #define subnet
 resource "aws_subnet" "tfsubnet" {
   vpc_id = "${aws_vpc.tftest.id}"
   cidr_block = "10.0.0.1/24"
+  map_public_ip_on_launch = "true"
 
   tags {
     Name = "TF Subnet"
   }
 }
+#define route
+resource "aws_route" "www" {
+  route_table_id = "${aws_vpc.tftest.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.gw.id}"
+}
+
+resource "aws_eip" "tftest-eip" {
+    vpc = "true"
+    depends_on = ["aws_internet_gateway.gw"]
+}
+
 
 resource "aws_instance" "Matt-Web-01" {
     ami = "ami-028779930ada5200c"
@@ -33,8 +55,6 @@ resource "aws_instance" "Matt-Web-01" {
     provisioner "local-exec" {
         command = "echo ${aws_instance.Matt-Web-01.public_ip} > ip_address.txt"
     }   
-
-
     tags {
         Name = "Matt-Web-01"
     }
@@ -64,6 +84,13 @@ resource "aws_security_group" "test-sg" {
     ingress {
         from_port = 5985
         to_port = 5985
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 3389
+        to_port = 3389
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
